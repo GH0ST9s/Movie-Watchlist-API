05beced6aa99dbc1ca564a8bcb876c0deb34664b
GH0ST9s <sushanstha523@gmail.com>
2026-09-23 13:57:53 +0545

    Changed user(id) to id

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const payload = { user: userId };
+  const payload = { id: userId };
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN || "7d",
   });
