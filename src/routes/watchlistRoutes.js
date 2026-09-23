7e2d44d04980197beae9cd0d6b480b376a088091
GH0ST9s <sushanstha523@gmail.com>
2026-09-23 14:03:37 +0545

    Added the authMiddleware

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {addToWatchlist} from "../controllers/watchlistController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
 router.post("/", addToWatchlist);
-// router.post("/login", login);
-// router.post("/logout", logout);
+
 
 export default router;
 
